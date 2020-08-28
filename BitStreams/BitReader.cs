fd83e27711c75eb5d3557f94043208dff53dbf2c
Stanko Culaja <stanko.culaja@kolotree.com>
2020-08-28 14:14:29 +0200

    Added project structure.

---
+++ BitStreams/BitReader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace BitStreams
+{
+    public sealed class BitReader
+    {
+        private readonly Stream _stream;
+
+        public BitReader(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public T Read<T>(int numberOfBits) where T : struct
+        {
+            return default;
+        }
+    }
+}
---
+++ BitStreamsTests/BitReaderTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace BitStreamsTests
+{
+    public class BitReaderTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
